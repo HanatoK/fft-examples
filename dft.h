c726923b33f6771977b40123c5cc2277cbf03716
hanatok <summersnow9403@gmail.com>
2020-07-19 15:48:13 -0500

    Add FFT examples and results.

--- dft.h
+++ dft.h
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES
 #include <complex>
 #include <vector>
 #include <iostream>
@@ -10,15 +11,16 @@ using std::complex;
 using std::vector;
 using std::cout;
 
+const size_t FFT_MIN_N = size_t(std::pow(2,4));
+
 vector<complex<double>> fourierTransform(const vector<complex<double>>& input) {
     using namespace std::complex_literals;
     const size_t N = input.size();
-    const double PI = std::acos(-1.0);
     vector<complex<double>> result(N);
     for (size_t k = 0; k < N; ++k) {
         complex<double> rN(0, 0);
         for (size_t j = 0; j < N; ++j) {
-            const double tmp = -1.0 * j * 2.0 * PI * k / N;
+            const double tmp = -1.0 * j * 2.0 * M_PI * k / N;
             rN += input[j] * (std::cos(tmp) + complex<double>(0, std::sin(tmp)));
         }
         result[k] = rN;
@@ -26,19 +28,110 @@ vector<complex<double>> fourierTransform(const vector<complex<double>>& input) {
     return result;
 }
 
+complex<double> fastFourierTransform(const vector<complex<double>>& input,
+                                     size_t k, size_t read_start, size_t read_stride, size_t N) {
+    using namespace std::complex_literals;
+    complex<double> rN(0, 0);
+    if (N % 2 != 0) {
+        for (size_t j = 0; j < N; ++j) {
+            const double tmp = -1.0 * j * 2.0 * M_PI * k / N;
+            rN += input[read_start + j * read_stride]
+                * (std::cos(tmp) + complex<double>(0, std::sin(tmp)));
+        }
+        return rN;
+    } else {
+        if (N <= FFT_MIN_N) {
+            const double factor = -1.0 * 2.0 * M_PI * k / N;
+            complex<double> rN_odd(0, 0);
+            complex<double> rN_even(0, 0);
+            for (size_t j = 0; j < N / 2; ++j) {
+                const double tmp = -1.0 * j * 2.0 * M_PI * k / (N / 2);
+                const complex<double> factor2 = std::cos(tmp) + complex<double>(0, std::sin(tmp));
+                rN_even += input[read_start + 2 * j * read_stride] * factor2;
+                rN_odd  += input[read_start + (2 * j + 1) * read_stride] * factor2;
+            }
+            rN = rN_even + rN_odd * (std::cos(factor) + complex<double>(0, std::sin(factor)));
+        } else {
+            const double factor = -1.0 * 2.0 * M_PI * k / N;
+            rN = fastFourierTransform(input, k, read_start, read_stride * 2, N / 2)
+               + fastFourierTransform(input, k, read_start + read_stride, read_stride * 2, N / 2)
+               * (std::cos(factor) + complex<double>(0, std::sin(factor)));
+        }
+        return rN;
+    }
+}
+
+vector<complex<double>> fastFourierTransform(const vector<complex<double>>& input) {
+    using namespace std::complex_literals;
+    const size_t N = input.size();
+    vector<complex<double>> result(N);
+    for (size_t k = 0; k < N; ++k) {
+        result[k] = fastFourierTransform(input, k, 0, 1, N);
+    }
+    return result;
+}
+
+vector<complex<double>> fastFourierTransform(const vector<double>& input) {
+    using namespace std::complex_literals;
+    vector<complex<double>> complex_input(input.size());
+    for (size_t i = 0; i < input.size(); ++i) {
+        complex_input[i] = complex<double>(input[i], 0.0);
+    }
+    return fastFourierTransform(complex_input);
+}
+
 vector<complex<double>> reverseFourierTransform(const vector<complex<double>>& input) {
     using namespace std::complex_literals;
     const size_t N = input.size();
-    const double PI = std::acos(-1.0);
     vector<complex<double>> result(N);
     for (size_t i = 0; i < N; ++i) {
         complex<double> rN(0, 0);
         for (size_t j = 0; j < N; ++j) {
-            double tmp = 1.0 * j * 2.0 * PI * i / N;
+            double tmp = 1.0 * j * 2.0 * M_PI * i / N;
             rN += input[j] * (std::cos(tmp) + complex<double>(0, std::sin(tmp)));
         }
-//         result[i] = rN / complex<double>(double(N), 0);
         result[i] = rN;
+    }
+    return result;
+}
+
+complex<double> reverseFastFourierTransform(const vector<complex<double>>& input, size_t k, size_t read_start, size_t read_stride, size_t N) {
+    using namespace std::complex_literals;
+    complex<double> rN(0, 0);
+    if (N % 2 != 0) {
+        for (size_t j = 0; j < N; ++j) {
+            const double tmp = 1.0 * j * 2.0 * M_PI * k / N;
+            rN += input[read_start + j * read_stride] * (std::cos(tmp) + complex<double>(0, std::sin(tmp)));
+        }
+        return rN;
+    } else {
+        if (N <= FFT_MIN_N) {
+            const double factor = 1.0 * 2.0 * M_PI * k / N;
+            complex<double> rN_odd(0, 0);
+            complex<double> rN_even(0, 0);
+            for (size_t j = 0; j < N / 2; ++j) {
+                const double tmp = 1.0 * j * 2.0 * M_PI * k / (N / 2);
+                const complex<double> factor2 = std::cos(tmp) + complex<double>(0, std::sin(tmp));
+                rN_even += input[read_start + 2 * j * read_stride] * factor2;
+                rN_odd  += input[read_start + (2 * j + 1) * read_stride] * factor2;
+            }
+            rN = rN_even + rN_odd * (std::cos(factor) + complex<double>(0, std::sin(factor)));
+        } else {
+            const double factor = 1.0 * 2.0 * M_PI * k / N;
+            rN = reverseFastFourierTransform(input, k, read_start, read_stride * 2, N / 2)
+               + reverseFastFourierTransform(input, k, read_start + read_stride, read_stride * 2, N / 2)
+               * (std::cos(factor) + complex<double>(0, std::sin(factor)));
+        }
+        return rN;
+    }
+}
+
+vector<complex<double>> reverseFastFourierTransform(const vector<complex<double>>& input) {
+    using namespace std::complex_literals;
+    const size_t N = input.size();
+    vector<complex<double>> result(N);
+    for (size_t k = 0; k < N; ++k) {
+        result[k] = reverseFastFourierTransform(input, k, 0, 1, N);
     }
     return result;
 }
@@ -65,13 +158,12 @@ vector<vector<complex<double>>> fourierTransform2D(const vector<complex<double>>
     using namespace std::complex_literals;
     const size_t N = inputX.size();
     const size_t M = inputY.size();
-    const double PI = std::acos(-1.0);
     vector<vector<complex<double>>> result(M, vector<complex<double>>(N));
     vector<complex<double>> resultX = fourierTransform(inputX);
     for (size_t j = 0; j < M; ++j) {
         complex<double> rM(0, 0);
         for (size_t jj = 0; jj < M; ++jj) {
-            double tmp = -1.0 * jj * 2.0 * PI * j / M;
+            double tmp = -1.0 * jj * 2.0 * M_PI * j / M;
             rM += inputY[jj] * (std::cos(tmp) + complex<double>(0, std::sin(tmp)));
         }
         for (size_t i = 0; i < N; ++i) {
@@ -112,6 +204,36 @@ vector<vector<complex<double>>> fourierTransform2D(const vector<vector<double>>&
     return result;
 }
 
+// fourier transform 
+vector<vector<complex<double>>> fastFourierTransform2D(const vector<vector<double>>& input) {
+    using namespace std::complex_literals;
+    const size_t N = input[0].size();
+    const size_t M = input.size();
+    vector<vector<complex<double>>> result(M, vector<complex<double>>(N));
+    // transform along X direction
+    for (size_t j = 0; j < M; ++j) {
+        result[j] = fastFourierTransform(input[j]);
+    }
+    // transpose
+    vector<vector<complex<double>>> tmp(N, vector<complex<double>>(M));
+    for (size_t i = 0; i < N; ++i) {
+        for (size_t j = 0; j < M; ++j) {
+            tmp[i][j] = result[j][i];
+        }
+    }
+    // transform along Y direction
+    for (size_t i = 0; i < N; ++i) {
+        tmp[i] = fastFourierTransform(tmp[i]);
+    }
+    // transpose back
+    for (size_t i = 0; i < N; ++i) {
+        for (size_t j = 0; j < M; ++j) {
+            result[j][i] = tmp[i][j];
+        }
+    }
+    return result;
+}
+
 vector<vector<complex<double>>> reverseFourierTransform2D(const vector<vector<complex<double>>>& input) {
     using namespace std::complex_literals;
     const size_t N = input[0].size();
@@ -131,6 +253,35 @@ vector<vector<complex<double>>> reverseFourierTransform2D(const vector<vector<co
     // reverse transform along Y direction
     for (size_t i = 0; i < N; ++i) {
         tmp[i] = reverseFourierTransform(tmp[i]);
+    }
+    // reverse transpose back
+    for (size_t i = 0; i < N; ++i) {
+        for (size_t j = 0; j < M; ++j) {
+            result[j][i] = tmp[i][j];
+        }
+    }
+    return result;
+}
+
+vector<vector<complex<double>>> reverseFastFourierTransform2D(const vector<vector<complex<double>>>& input) {
+    using namespace std::complex_literals;
+    const size_t N = input[0].size();
+    const size_t M = input.size();
+    vector<vector<complex<double>>> result(M, vector<complex<double>>(N));
+    // reverse transform along X direction
+    for (size_t j = 0; j < M; ++j) {
+        result[j] = reverseFastFourierTransform(input[j]);
+    }
+    // transpose
+    vector<vector<complex<double>>> tmp(N, vector<complex<double>>(M));
+    for (size_t i = 0; i < N; ++i) {
+        for (size_t j = 0; j < M; ++j) {
+            tmp[i][j] = result[j][i];
+        }
+    }
+    // reverse transform along Y direction
+    for (size_t i = 0; i < N; ++i) {
+        tmp[i] = reverseFastFourierTransform(tmp[i]);
     }
     // reverse transpose back
     for (size_t i = 0; i < N; ++i) {
